c663a3da2b3d918754451988122a292175f2dbae
tomrob270 <38351556+tomrob270@users.noreply.github.com>
2023-09-28 13:15:52 +0100

    Add project files.

---
+++ RailPen/Data/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RailPen.Models;
+using System.Globalization;
+using static RailPen.Models.Member;
+
+namespace RailPen.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Member> Members { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var cultureInfo = new CultureInfo("en-GB");
+
+            modelBuilder.Entity<Member>().HasData(
+                new Member { Id = 1, FirstName = "Brian", LastName = "Mccall", PensionReferenceNumber = "P0000097", DateOfBirth = DateTime.Parse("01/08/1965", cultureInfo), Gender = GenderEnum.Male, TargetRetirementAge = 60 },
+                new Member { Id = 2, FirstName = "Sarah", LastName = "Kramer", PensionReferenceNumber = "PO000136", DateOfBirth = DateTime.Parse("03/04/1984", cultureInfo), Gender = GenderEnum.Female, TargetRetirementAge = 60 },
+                new Member { Id = 3, FirstName = "Lianne", LastName = "Barrow", PensionReferenceNumber = "P0004657", DateOfBirth = DateTime.Parse("26/09/1975", cultureInfo), Gender = GenderEnum.Female, TargetRetirementAge = 62 },
+                new Member { Id = 4, FirstName = "David", LastName = "Richardson", PensionReferenceNumber = "P0004701", DateOfBirth = DateTime.Parse("25/12/1980", cultureInfo), Gender = GenderEnum.Male, TargetRetirementAge = 65 },
+                new Member { Id = 5, FirstName = "Kara", LastName = "Mccall", PensionReferenceNumber = "P0004782", DateOfBirth = DateTime.Parse("06/05/1992", cultureInfo), Gender = GenderEnum.Female, TargetRetirementAge = 65 }
+            );
+        }
+    }
+}
---
+++ RailPen/Models/Member.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RailPen.Models
+{
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Pension Reference Number")]
+        public string PensionReferenceNumber { get; set; }
+
+        [Required]
+        [DisplayName("Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        public enum GenderEnum
+        {
+            Male,
+            Female
+        }
+
+        [Required]
+        public GenderEnum Gender { get; set; }
+
+        [Required]
+        public int TargetRetirementAge { get; set; }
+        
+        public DateTime DateOfRetirement 
+        { 
+            get
+            {
+                return DateOfBirth.AddYears(TargetRetirementAge);
+            }
+            set {}
+        }
+    }
+}
